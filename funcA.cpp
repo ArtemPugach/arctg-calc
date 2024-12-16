441aa808abfec1781d2ee534d08cc6682a2535c7
Artem Pugach <pugacartem106@gmail.com>
2024-12-16 19:26:18 +0200

    Hotfix: calculate sum of 3 elements

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,13 @@
 #include "funcA.h"
+#include <cmath>
 
 double FuncA::calculate() {
-    return 0.0;  // Поки що повертаємо 0
+    int n = 3;  // Фіксоване значення n = 3
+    double result = 0.0;
+    for (int i = 0; i < n; ++i) {
+        double term = pow(-1, i) * pow(1.0, 2*i + 1) / (2*i + 1);
+        result += term;
+    }
+    return result;
 }
 
