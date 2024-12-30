5829fefba23df857097b7a814b8cb6950fc4ea54
Artem Pugach <pugacartem106@gmail.com>
2024-12-30 20:33:10 +0200

    Refactored ArthCalculator into header file in branchMake

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
-#include <cmath>
-#include <stdexcept>
-
-class ArthCalculator {
-public:
-    double arth(double x, int terms) {
-        if (std::abs(x) >= 1) {
-            throw std::invalid_argument("x must be in the range -1 < x < 1");
-        }
-        double result = 0.0;
-        for (int n = 0; n < terms; ++n) {
-            result += std::pow(x, 2 * n + 1) / (2 * n + 1);
-        }
-        return result;
-    }
-};
+#include "ArthCalculator.h"
 
 int main() {
     ArthCalculator calculator;
